e7e59949ee3aff3e601f14618254d685bc5756af
Marina Nikolaeva <marinan97@bk.ru>
2026-06-11 13:08:19 +0400

    Add dotenv support

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print('Hello from repository!')  
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def print_author():
+    author = os.getenv("AUTHOR")
+    print(f"Автор проекта: {author}")
+
+print_author() 
+
